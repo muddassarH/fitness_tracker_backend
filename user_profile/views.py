6f6669093a6c245e338172f2b75ddf1cac0e7fa7
muddassarH <muddassarhussain90@gmail.com>
2025-06-19 18:32:22 +0500

    fetch user profile view.

--- user_profile/views.py
+++ user_profile/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
 
-# Create your views here.
+class FetchUserProfileView(APIView):
+    def get(self, request):
+        # Logic to retrieve user profile data
+        user = request.user
+        context = {
+            'username': user.username,
+            'email': user.email,
+            'age': user.age,
+            'goals': user.goals,
+            'profile_picture': user.profile_picture.url if user.profile_picture else None,
+            'location': user.location,
+            'birth_date': user.birth_date,
+        }
+        return Response(context, status=status.HTTP_200_OK)
+    
+class CreateUserProfileView(APIView):
+    def post(self, request):
+        pass
